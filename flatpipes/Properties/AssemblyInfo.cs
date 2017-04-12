e4cc8117135ab1a3041df3e2f861bec67feefac2
dxflatline <dc.flatline@gmail.com>
2017-04-12 20:56:18 +0300

    Initial commit

--- flatpipes/Properties/AssemblyInfo.cs
+++ flatpipes/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("namedpipeboth")]
+[assembly: AssemblyTitle("flatpipes")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("namedpipeboth")]
+[assembly: AssemblyCompany("dxflatline")]
+[assembly: AssemblyProduct("flatpipes")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
